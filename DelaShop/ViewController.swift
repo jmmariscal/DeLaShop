e94da5694680fe7a9feeb1ddcc17163ef8a67ccd
Juan M Mariscal <juanmmariscal@juans-macbook-pro.local>
2018-03-19 14:12:28 -0700

    Initial Commit

---
+++ DelaShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelaShop
+//
+//  Created by Juan M Mariscal on 3/19/18.
+//  Copyright © 2018 Juan M Mariscal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DelaShopTests/DelaShopTests.swift
@@ -0,0 +1,36 @@
+//
+//  DelaShopTests.swift
+//  DelaShopTests
+//
+//  Created by Juan M Mariscal on 3/19/18.
+//  Copyright © 2018 Juan M Mariscal. All rights reserved.
+//
+
+import XCTest
+@testable import DelaShop
+
+class DelaShopTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
